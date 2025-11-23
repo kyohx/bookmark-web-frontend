9af56453a49b734b4947aa7715c0cb53c00e12bb
Keitaro Kimura <kyohxyz@gmail.com>
2025-11-23 19:43:50 +0900

    feat: テスト用のTypeScript設定とVitestおよびTesting Libraryの型定義を追加

--- tests/vitest.d.ts
+++ tests/vitest.d.ts
@@ -1,1 +1,9 @@
+/// <reference types="@testing-library/jest-dom" />
+
 import '@testing-library/jest-dom/vitest';
+import type { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers';
+
+declare module 'vitest' {
+    interface Assertion<T = unknown> extends jest.Matchers<void, T>, TestingLibraryMatchers<T, void> { }
+    interface AsymmetricMatchersContaining extends jest.Matchers<void, unknown> { }
+}
