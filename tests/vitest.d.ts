4f52549b22e01464070dfd23d5c5f740e253fa50
Keitaro Kimura <kyohxyz@gmail.com>
2025-11-23 19:47:30 +0900

    refactor: Vitest型定義から`AsymmetricMatchersContaining`インターフェースの宣言を削除

--- tests/vitest.d.ts
+++ tests/vitest.d.ts
@@ -5,5 +5,4 @@ import type { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers'
 
 declare module 'vitest' {
     interface Assertion<T = unknown> extends jest.Matchers<void, T>, TestingLibraryMatchers<T, void> { }
-    interface AsymmetricMatchersContaining extends jest.Matchers<void, unknown> { }
 }
